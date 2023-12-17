a374414c7b0c5e9f46b2192bd0eff83d18858d5a
AlejandraKantun27 <kantunalexa278@gmail.com>
2023-12-17 16:02:23 -0600

    fix 2

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -67,7 +67,7 @@ class AuthController {
 
       res.json({ userId });
     } catch (error) {
-      res.status(401).json({ message: 'Token inválido', error: error.message });
+      res.status(401).json({ message: 'Token inválido', error: error });
     }
   }
 
